8efb8ff2b417417344aa1bdfb1c842466cdec1c7
snehapaul2005 <snehapaul1871@gmail.com>
2025-04-02 23:27:30 +0530

    Add files via upload

---
+++ size of int,flaot,double,char.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("The size of integer is %u bytes\n",sizeof(int));
+	printf("The size of integer is %u bytes\n",sizeof(float));
+	printf("The size of integer is %u bytes\n",sizeof(double));
+	printf("The size of integer is %u bytes\n",sizeof(char));
+	return 0;
+}
