b5be4614edf00ac40b9806d7d2306f7fe47ccafd
snehapaul2005 <snehapaul1871@gmail.com>
2025-04-02 22:48:09 +0530

    Add files via upload

---
+++ ceiling and floor of a +ve & -ve.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float a;
+	printf("Enter a:");
+	scanf("%f",&a);
+	printf("The ceiling value of a is %f\n",ceil(a));
+	printf("The floor value of a is %f\n",floor(a));
+	return 0;
+}
