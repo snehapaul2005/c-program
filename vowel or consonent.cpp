5ed7e6a843915cfed39060793761acbe917441e7
snehapaul2005 <snehapaul1871@gmail.com>
2025-04-02 23:26:16 +0530

    Add files via upload

---
+++ vowel or consonent.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	char x,a,e,i,o,u;
+	printf("Enter the alphabet:");
+	scanf("%c",&x);
+	if(x=='a')
+	{
+		printf("The given alphabet is vowel");
+	}
+	else if(x=='e')
+	{
+		printf("The given alphabet is vowel");	
+	}
+	else if(x=='i')
+	{
+		printf("The given alphabet is vowel");	
+	}
+	else if(x=='o')
+	{
+		printf("The given alphabet is vowel");	
+	}
+	else if(x=='u')
+	{
+		printf("The given alphabet is vowel");	
+	}
+	else
+	{
+		printf("The given alphabet is consonent");	
+	}
+	return 0;
+}
