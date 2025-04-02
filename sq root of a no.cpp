4652c9b55252e6bda76f04b329acd013004aa89e
snehapaul2005 <snehapaul1871@gmail.com>
2025-04-02 22:49:20 +0530

    Add files via upload

---
+++ sq root of a no.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	float a;
+	printf("Enter a number:");
+	scanf("%f",&a);
+	printf("The square root of the given value is %f",sqrt(a));
+	return 0;
+}
