4a1b39045fffebe11064c74a9ac1275d9c2f28b0
snehapaul2005 <snehapaul1871@gmail.com>
2025-04-02 22:50:12 +0530

    Add files via upload

---
+++ sum of sq of ist 10 natural no.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int a;
+	a=pow(1,2)+pow(2,2)+pow(3,2)+pow(4,2)+pow(5,2)+pow(6,2)+pow(7,2)+pow(8,2)+pow(9,2)+pow(10,2);
+	printf("The sum of the square of first 10 natural numbers is %d",a);
+	return 0;
+}
