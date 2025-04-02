48ee1424619937cdd8303e3288cfe3a97ae0ebff
snehapaul2005 <snehapaul1871@gmail.com>
2025-04-02 22:51:59 +0530

    Add files via upload

---
+++ odd even (if else).cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter n=");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+		printf("n is even");
+	}
+	else
+	{
+     	printf("n is odd");
+		
+	}
+	return 0;
+}
