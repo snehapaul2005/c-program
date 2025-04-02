7cb0b48252ed9fc5cf5a9e52b792fa8fdde232fc
snehapaul2005 <snehapaul1871@gmail.com>
2025-04-03 02:04:15 +0530

    Add files via upload

---
+++ pattern 2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,k,count=1;
+	for(i=1;i<=4;i++)
+	{
+		for(k=1;k<=4-i;k++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",count);
+			count++;
+		}
+		printf("\n");
+	}
+	return 0;
+}
